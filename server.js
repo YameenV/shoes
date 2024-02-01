ae3bb1cc1e6e533a5203813abb76c8474d103a89
YameenV <yameenvinchu38@gmail.com>
2024-02-01 10:01:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+
+//config env
+dotenv.config();
+
+
+//datbase config
+connectDB();
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth', authRoutes)
+
+//rest api
+app.get('/', (req,res) => {
+    res.send('<h1>Welcome to Faiz Shoes</h1>')
+})
+
+//PORT
+const PORT = process.env.PORT;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
